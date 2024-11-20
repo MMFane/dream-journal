2c1ff87a8cc08c7720f2d6732e6b18788632869e
MMFane <sam.erin.williams@gmail.com>
2024-11-19 21:30:32 -0800

    add dummy DreamsList

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import DreamsList from './components/DreamsList.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <DreamsList />
 </template>
 
 <style scoped>
---
+++ src/components/DreamsList.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import dreams from "../data/data.json"
+</script>
+
+<template>
+    <h1>Dream List</h1>
+    <li v-for="dream in dreams">{{ dream.date }}: {{ dream.description }}</li>
+</template>
