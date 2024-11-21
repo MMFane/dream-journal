504aeb60c3e0ad6234fdfe72019f8a433f724fe8
MMFane <sam.erin.williams@gmail.com>
2024-11-20 18:26:09 -0800

    style nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/dreams">Dreams</RouterLink>
-    <RouterLink to="/tags">Tags</RouterLink>
-    <RouterLink to="/visualizations">Visualizations</RouterLink>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <v-layout>
+    <v-navigation-drawer expand-on-hover rail :mobile="false">
+      <v-list>
+        <v-list-item prepend-icon="mdi-home"
+          ><RouterLink to="/">Home</RouterLink></v-list-item
+        >
+        <v-list-item prepend-icon="mdi-weather-night"
+          ><RouterLink to="/dreams">Dreams</RouterLink></v-list-item
+        >
+        <v-list-item prepend-icon="mdi-tag"
+          ><RouterLink to="/tags">Tags</RouterLink></v-list-item
+        >
+        <v-list-item prepend-icon="mdi-chart-box-multiple-outline"
+          ><RouterLink to="/visualizations"
+            >Visualizations</RouterLink
+          ></v-list-item
+        >
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-layout>
 </template>
 
 <style scoped></style>
