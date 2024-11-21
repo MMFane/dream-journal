f0fae4653f9b1111651b05637bb1eae084b5eaf8
MMFane <sam.erin.williams@gmail.com>
2024-11-20 19:25:41 -0800

    change and style chip list

---
+++ src/components/ChipList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { VChip } from 'vuetify/components'
+interface ChipListProps {
+  itemList: string
+  variant?: VChip['variant']
+}
+const props = defineProps<ChipListProps>()
+const displayList = props.itemList.split(', ')
+</script>
+
+<template>
+  <div class="chipList">
+    <v-chip v-for="item in displayList" :key="item" :variant="variant">{{
+      item
+    }}</v-chip>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.chipList {
+  padding-top: 1rem;
+}
+</style>
