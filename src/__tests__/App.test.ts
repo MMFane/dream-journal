ce3a646c5b626fd5746f5dafe139d4a3fdb83ce0
MMFane <sam.erin.williams@gmail.com>
2024-11-27 12:27:02 -0800

    add before, after, and plugin to App test

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -1,10 +1,27 @@
-import { describe, it, expect } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { describe, it, expect, afterEach, beforeEach } from 'vitest'
+import { mount, VueWrapper } from '@vue/test-utils'
 import App from '../App.vue'
+import vuetifyPlugin from '../vuetify'
+import { Plugin } from 'vue'
 
 describe('App', () => {
+  let wrapper: VueWrapper
+  let vuetify: Plugin
+  beforeEach(() => {
+    vuetify = vuetifyPlugin
+    wrapper = mount(App, {
+      global: {
+        plugins: [vuetify]
+      },
+      props: {}
+    })
+  })
+
+  afterEach(() => {
+    wrapper.unmount()
+  })
+
   it('renders correctly', () => {
-    const wrapper = mount(App)
     expect(wrapper.text()).toContain('Dreams')
     expect(wrapper.text()).toContain('Tags')
     expect(wrapper.text()).toContain('Visualizations')
