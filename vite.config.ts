cd0816902656c551634ba326fb912265e9d11f26
MMFane <sam.erin.williams@gmail.com>
2024-11-27 12:16:38 -0800

    update test config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })]
+  plugins: [vue(), vuetify({ autoImport: true })],
+  test: {
+    environment: 'happy-dom',
+    globals: true
+  },
+  ssr: {
+    noExternal: ['vuetify']
+  }
 })
